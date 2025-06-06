e1d5539d22c784f6370af8a83585cfd628f1b6e9
CaioGotardo <caiogotardo.dev@gmail.com>
2025-06-06 20:44:47 -0300

    finalizado

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,5 +9,22 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+//valor total emp.
+v:number = 0;
+  // juros 
+i:number = 0;
+// meses
+n:number = 0;
+//valor parcela 
+p:number = 0;
+
+calculoParcela(){
+  this.p= this.v*(this.i/12)/1-(1+this.i)**-this.n;
+  this.p = Math.round(this.p);
+
+}
+
+
+
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,5 +9,33 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   constructor() {}
+//valor inicial inv.
+p:number = 0;
+  // taxa retorno anual 
+r:number = 0;
+// periodo investido
+t:number = 0;
+//montante
+m:number=0
+//n
+n: number = 1; 
 
+
+calculo(){
+
+    if (this.p <= 0 || this.r < 0 || this.n <= 0 || this.t <= 0) {
+      console.warn("Por favor, insira valores válidos para P, r, n e t.");
+      this.m = 0;
+      return;
+    }
+
+  
+    const taxaDecimal = this.r / 100;
+    
+    this.m = this.p * Math.pow((1 + (taxaDecimal / this.n)), (this.n * this.t));
+    this.m = parseFloat(this.m.toFixed(2));
+  }
 }
+
+
+
