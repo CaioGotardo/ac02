63173e088a1be0a71bebf48eee633e0e8075fae2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-06-09 09:27:29 -0300

    1 e 2 finalizado

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,24 +8,36 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab1Page {
-
   constructor() {}
-//valor total emp.
-v:number = 0;
-  // juros 
-i:number = 0;
-// meses
-n:number = 0;
-//valor parcela 
-p:number = 0;
+  //valor total emp.
+  v: number = 0;
+  // juros
+  i: number = 0;
+  // meses
+  n: number = 0;
+  //valor parcela
+  p: number = 0;
 
-calculoParcela(){
-  this.p= this.v*(this.i/12)/1-(1+this.i)**-this.n;
-  this.p = Math.round(this.p);
+  // alerta btn
+  alertButtons = ['OK'];
 
-}
-
+  mensagem: any;
 
 
 
-}
+  calculoParcela() {
+    this.p = (this.v * (this.i / 12)) / 1 - (1 + this.i) ** -this.n;
+    this.p = Math.round(this.p);
+
+    if(this.p > 20){
+      this.mensagem = 'Cuidado'
+    }else{
+      this.mensagem = 'fica em paz '
+    }
+  }
+
+
+  }
+
+
+
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -11,16 +11,18 @@ export class Tab2Page {
   constructor() {}
 //valor inicial inv.
 p:number = 0;
-  // taxa retorno anual 
+  // taxa retorno anual
 r:number = 0;
 // periodo investido
 t:number = 0;
 //montante
 m:number=0
 //n
-n: number = 1; 
+n: number = 1;
 
+mensagem:any;
 
+alertButtons = ['OK']
 calculo(){
 
     if (this.p <= 0 || this.r < 0 || this.n <= 0 || this.t <= 0) {
@@ -29,12 +31,21 @@ calculo(){
       return;
     }
 
-  
+
     const taxaDecimal = this.r / 100;
-    
+
     this.m = this.p * Math.pow((1 + (taxaDecimal / this.n)), (this.n * this.t));
     this.m = parseFloat(this.m.toFixed(2));
+
+
+
+    if(this.m > 15){
+      this.mensagem = "RISCO DE INVESTIMENTO"
+    }else{
+      this.mensagem = "FICA EM PAZ "
+    }
   }
+
 }
 
 
